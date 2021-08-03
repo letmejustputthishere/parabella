1b55bcbe19756c7d6e62de3183b64bfe9af7a4ef
Moritz Fuller <fullermoritz@gmail.com>
2021-08-03 19:31:58 +0200

    enabling default path

--- src/App.svelte
+++ src/App.svelte
@@ -30,9 +30,9 @@
 
 <Router {url}>
 	<!-- <Route path="/"><Survey {language} bind:answers/></Route> -->
-	<Route path="/"><Dashboard {language} bind:answers /></Route>
+	<!-- <Route path="/"><Dashboard {language} bind:answers /></Route> -->
 
-	<!-- <Route path="/"><LandingPage {language} /></Route> -->
+	<Route path="/"><LandingPage {language} /></Route>
 	<Route path="survey"><Survey {language} bind:answers /></Route>
 	<Route path="dashboard"><Dashboard {language} {answers} /></Route>
 </Router>
